f3b99813409883257b87e10a79ff8198b57f871b
Juli Tay <119788019+julitays@users.noreply.github.com>
2025-07-30 16:30:18 +0300

    2 commit

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 1. Укажи свои данные подключения
-DATABASE_URL = "postgresql://postgres:rootroot@localhost/rolesmaster"
 
+# 1. Укажи свои данные подключения
+DATABASE_URL="postgresql://postgres.lyfqjutojeqnyvlqennc:Rootroot123!@aws-0-eu-west-1.pooler.supabase.com:6543/postgres"
 # Заменить на твои реальные значения
 # Пример:
 # DATABASE_URL = "postgresql://postgres:1234@localhost/hr_roles"
@@ -18,3 +18,4 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 4. Базовый класс для моделей
 Base = declarative_base()
+
--- init_bd.py
+++ init_bd.py
@@ -1,5 +1,12 @@
 # init_db.py
+
 from database import engine
 from models import Base
 
+print("Инициализация базы...")
+print(engine.url)
+
+# Создание всех таблиц в облачной базе Supabase
 Base.metadata.create_all(bind=engine)
+
+print("Таблицы должны быть созданы!")
